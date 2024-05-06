d8aa52171f928713556203718c9b7098d5dfc672
D4NILO07 <161032185+D4NILO07@users.noreply.github.com>
2024-05-06 14:20:09 +0000

    first python

---
+++ 6ºQuestão.py
@@ -0,0 +1,12 @@
+import os
+os.system("cls || clear")
+
+
+
+login = input("Digite seu email: ")
+senha = input("Digite sua senha: ")
+
+if login == "daniloalmeidapaulo@gmail.com" and senha == "1002": 
+    print("Seja Bem - Vindo")
+else: 
+    print("Email ou senha incorretos.")
